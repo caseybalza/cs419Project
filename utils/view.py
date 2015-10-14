285b9c967721eb46ec66e97d9281d0c57e8a78da
jurczakn <jurczakn@onid.oregonstate.edu>
2015-10-14 06:05:12 -0500

    NcursesHandler/Form test
    -createdNcursesHandler.py
    -initializes ncurses, startmenu(), processmenu(), runmenu(), runform(),
    resetscreen(), exit_window()[notworking], exit_program()[not working]
    -view.py added login_form menu and FORM and TEXT to Dictionaries
    -main.py edited to use NCursesHandler

--- utils/view.py
+++ utils/view.py
@@ -2,6 +2,20 @@ class Dictionary:
 	MENU = "menu"
 	COMMAND = "command"
 	EXITMENU = "exitmenu"
+	FORM = "form"
+	TEXT = "text"
+
+login_form = {
+	'title': "Login", 'type': Dictionary.FORM, 'subtitle': "Please login to your db account",
+	'fields':[
+		{'title': "Username: ", 'type': Dictionary.TEXT},
+		{'title': "Password: ", 'type': Dictionary.TEXT}
+	],
+	'options': [
+		{'title': "Back", 'type': Dictionary.COMMAND, 'command': 'processMenu(main_menu)'},
+		{'title': "Continue", 'type': Dictionary.COMMAND, 'command': 'testfun()'}
+	]
+}
 
 
 main_menu = {
@@ -9,6 +23,7 @@ main_menu = {
 	'options':[
   		{ 'title': "Use MySQL databases", 'type': Dictionary.COMMAND, 'command': 'use_mysql()' },
     	{ 'title': "Use PostgreSQL databases", 'type': Dictionary.COMMAND, 'command': 'use_psql()' },
+	{ 'title': "Login Test", 'type': Dictionary.COMMAND, 'command': 'login(ncurses)' },
 		#Menu with a submenu
         { 'title': "Sebmenu preview", 'type': Dictionary.MENU, 'subtitle': "Please select an option...",
         	'options': [
@@ -23,7 +38,7 @@ main_menu = {
 exit_menu = {
 	'title': "Exit program?", 'type': Dictionary.MENU, 'subtitle': "Please select an action...",
 	'options':[
-  		{ 'title': "Yes", 'type': Dictionary.COMMAND, 'command': 'exit_program()' },
+  		{ 'title': "Yes", 'type': Dictionary.COMMAND, 'command': 'exit_program(ncurses)' },
   ]#end of exit_menu options
 }#end of exit_menu data
 
